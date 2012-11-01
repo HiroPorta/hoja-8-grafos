cf7e66a1b436a25762e09bfe5be8c2565f53dd60
mar11176 <mar11176@uvg.edu.gt>
2012-11-01 21:00:29 +0000

    Implementación del algoritmo de Floyd

--- Hoja8/src/hoja8/Floyd.java
+++ Hoja8/src/hoja8/Floyd.java
@@ -6,30 +6,56 @@ package hoja8;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Maria Fernanda Martinez, Adrian Martinez
+ * @author Mafer
  */
 public class Floyd {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args){
-        // TODO code application logic here
-        Archivo a = new Archivo();
-        try{
+    Archivo a= new Archivo();
+    int[][] D;
+    int[][] P;
+    
+    public Floyd(){
+        try {
             a.obtenerArchivo("C:/Users/Mafer/Documents/Universidad del Valle de Guatemala/Cuarto semestre/Algoritmos y Estructuras de Datos/Hoja de trabajo 8/Hoja8/src/hoja8/datos.txt");
             a.arregloNombres();
-            a.matrizCostos();
+            D = a.matrizCostos();
+            P = new int[a.elementos][a.elementos];
         }catch(FileNotFoundException e){
             e.printStackTrace();
             System.out.println("No se ha encontrado el archivo");
-        }catch(IOException e){
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        for(int n=0;n<a.elementos;n++){
+            for(int m=0;m<a.elementos;m++){
+                System.out.println("m "+m+" n "+n);
+                P[n][m]=0;
+            }
+        }
+    }
+    
+    public void caminoCorto(){
+        for(int k=0;k<a.elementos;k++){
+            for(int i=0;i<a.elementos;i++){
+                for(int j=0;j<a.elementos;j++){
+                    if(D[i][j]>(D[i][k]+D[k][j])){
+                        D[i][j]=D[i][k]+D[k][j];
+                        P[i][j]=k;
+                    }
+                }
+            }
         }
         
-        
+        for(int i=0;i<a.elementos;i++){
+            for(int j=0;j<a.elementos;j++){
+                System.out.print(""+D[i][j]+" ");
+            }
+            System.out.println("");
+        }
     }
 }
+    
