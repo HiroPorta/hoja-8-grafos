de8421b110b46017d2b6a8a83a8f8b3db7370193
mar11176@uvg.edu.gt <mar11176@uvg.edu.gt@3bfa7488-8542-639f-ac65-b78c4fc6ca12>
2012-11-03 01:36:57 +0000

    Implementación de InterfazGrafos con GraphMatrix, utilización de grafos para guardar datos de archivo

--- Hoja8/src/hoja8/Floyd.java
+++ Hoja8/src/hoja8/Floyd.java
@@ -4,58 +4,50 @@
  */
 package hoja8;
 
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
- * @author Mafer
+ * @author Maria Fernanda Martinez, Adrian Martinez
  */
 public class Floyd {
     Archivo a= new Archivo();
-    int[][] D;
+    InterfazGrafo D;
     int[][] P;
+    InterfazGrafo grafo = new GraphMatrix();
     
     public Floyd(){
+        
         try {
             a.obtenerArchivo("C:/Users/Mafer/Documents/Universidad del Valle de Guatemala/Cuarto semestre/Algoritmos y Estructuras de Datos/Hoja de trabajo 8/Hoja8/src/hoja8/datos.txt");
             a.arregloNombres();
             D = a.matrizCostos();
-            P = new int[a.elementos][a.elementos];
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-            System.out.println("No se ha encontrado el archivo");
+            P = new int[a.grafo.size()][a.grafo.size()];
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        for(int n=0;n<a.elementos;n++){
-            for(int m=0;m<a.elementos;m++){
-                System.out.println("m "+m+" n "+n);
+        for(int n=0;n<a.grafo.size();n++){
+            for(int m=0;m<a.grafo.size();m++){
                 P[n][m]=0;
             }
         }
     }
     
     public void caminoCorto(){
-        for(int k=0;k<a.elementos;k++){
-            for(int i=0;i<a.elementos;i++){
-                for(int j=0;j<a.elementos;j++){
-                    if(D[i][j]>(D[i][k]+D[k][j])){
-                        D[i][j]=D[i][k]+D[k][j];
+        for(int k=0;k<a.grafo.size();k++){
+            for(int i=0;i<a.grafo.size();i++){
+                for(int j=0;j<a.grafo.size();j++){
+                    if(D.getEdge(a.grafo.get(i),a.grafo.get(j))>(D.getEdge(a.grafo.get(i), a.grafo.get(k))+D.getEdge(a.grafo.get(k), a.grafo.get(j)))){
+                        D.addEdge(a.grafo.get(i), a.grafo.get(j), (D.getEdge(a.grafo.get(i), a.grafo.get(k))+D.getEdge(a.grafo.get(k), a.grafo.get(j))));
                         P[i][j]=k;
                     }
                 }
             }
         }
-        
-        for(int i=0;i<a.elementos;i++){
-            for(int j=0;j<a.elementos;j++){
-                System.out.print(""+D[i][j]+" ");
-            }
-            System.out.println("");
-        }
+        System.out.println("Matriz final: ");
+        D.show();
     }
 }
+
     
